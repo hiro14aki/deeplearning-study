33edb46de382946e67778ec4a609e5939c3492e9
Hiroaki Imai <hiro.1114.aki@gmail.com>
2017-05-13 23:40:28 +0900

    XORゲートのお試し

--- perceptron.py
+++ perceptron.py
@@ -1,12 +1,43 @@
+import numpy as np
+
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1 * w1 + x2 * w2
-    if tmp <= theta:
+    x = np.array([x1, x2])
+    y = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x * y) + b
+    if tmp <= 0:
         return 0
-    elif tmp > theta:
+    else:
         return 1
 
-print(AND(0, 0))
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    y = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x * y) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    y = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x * y) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
 print(AND(1, 0))
-print(AND(0, 1))
-print(AND(1, 1))
+print(NAND(1, 0))
+print(OR(1, 0))
+print(XOR(1, 1))
