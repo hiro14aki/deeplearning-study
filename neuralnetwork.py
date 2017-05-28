cae9e6576d19989c67be4bde6d55995148f73ea0
Hiroaki Imai <hiro.1114.aki@gmail.com>
2017-05-29 02:26:34 +0900

    3層ニューラルネットワークの書き方を関数に変更

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -8,45 +8,32 @@ def sigmoid(x):
 def identity_function(x):
         return x
 
-# 1階層目の重み付け
-X = np.array([1.0, 0.5])
-W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-B1 = np.array([0.1, 0.2, 0.3])
 
-print(W1.shape) # (2, 3)
-print(X.shape)  # (2,)
-print(B1.shape) # (3,)
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
 
-# 1階層目の重み付き和
-A1 = np.dot(X, W1) + B1
-print(A1)
+    return network
 
-# 1階層目の活性化関数
-Z1 = sigmoid(A1)
-print(Z1)
+def forward(network, X):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-# 2層目の重み付け
-W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-B2 = np.array([0.1, 0.2])
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
 
-print(Z1.shape) # (3,)
-print(W2.shape) # (3, 2)
-print(B2.shape) # (2,)
+    return y
 
-# 2階層目の重み付け和
-A2 = np.dot(Z1, W2) + B2
-
-# 2階層目の活性化関数
-Z2 = sigmoid(A2)
-print(Z2)
-
-# 3階層目の重み付け
-W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
-B3 = np.array([0.1, 0.2])
-
-# 2階層目の重み付け和
-A3 = np.dot(Z2, W3) + B3
-
-# 3階層目の恒等関数
-Y = identity_function(A3)
-print(Y)
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
