2a9bee201d93c61c21b048fa4e6c505a99598fb2
hiro14aki <hiro.1114.aki@gmail.com>
2023-11-11 01:25:55 +0900

    feat: chapter1 の python チュートリアル実施 - numpy - matplotlib

---
+++ chapter1_onboarding.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+# sin グラフ
+x = np.arange(0, 6, 0.1)
+y = np.sin(x)
+plt.plot(x, y)
+plt.show()
+
+# cos グラフ
+x = np.arange(0, 6, 0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('sin & cos')
+plt.legend()
+plt.show()
+
+# Image
+img = imread('./assets/tennis.jpeg')
+plt.imshow(img)
+plt.show()
