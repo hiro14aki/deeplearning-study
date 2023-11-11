e607f40ae8c50407d7b55d89ffb328a6b7a6b7eb
hiro14aki <hiro.1114.aki@gmail.com>
2023-11-11 09:38:56 +0900

    feat: Implement NAND and OR gate.

--- chapter2/perceptron.py
+++ chapter2/perceptron.py
@@ -10,12 +10,11 @@ def and_gate(x1, x2):
         return 1
 
 
+print('-- AND Gate --')
 print(and_gate(0, 0))
 print(and_gate(1, 0))
 print(and_gate(0, 1))
 print(and_gate(1, 1))
-
-print('-----')
 
 
 def and_gate_with_numpy(x1, x2):
@@ -29,7 +28,44 @@ def and_gate_with_numpy(x1, x2):
         return 1
 
 
+print('-- AND Gate with Numpy --')
 print(and_gate_with_numpy(0, 0))
 print(and_gate_with_numpy(1, 0))
 print(and_gate_with_numpy(0, 1))
 print(and_gate_with_numpy(1, 1))
+
+
+def nand_gate(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print('-- NAND Gate --')
+print(nand_gate(0, 0))
+print(nand_gate(1, 0))
+print(nand_gate(0, 1))
+print(nand_gate(1, 1))
+
+
+def or_gate(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print('-- OR Gate --')
+print(or_gate(0, 0))
+print(or_gate(1, 0))
+print(or_gate(0, 1))
+print(or_gate(1, 1))
