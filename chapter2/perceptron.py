3edcca235c5c4ce2738da3bb3998dbdca8119d65
hiro14aki <hiro.1114.aki@gmail.com>
2023-11-11 09:29:50 +0900

    feat: Implemented AND gate with Numpy.

--- chapter2/perceptron.py
+++ chapter2/perceptron.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def and_gate(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
     tmp = x1 * w1 + x2 * w2
@@ -11,3 +14,22 @@ print(and_gate(0, 0))
 print(and_gate(1, 0))
 print(and_gate(0, 1))
 print(and_gate(1, 1))
+
+print('-----')
+
+
+def and_gate_with_numpy(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print(and_gate_with_numpy(0, 0))
+print(and_gate_with_numpy(1, 0))
+print(and_gate_with_numpy(0, 1))
+print(and_gate_with_numpy(1, 1))
