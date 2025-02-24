4097e5ffbddf78f90455bf289b869dcb37df2ef9
hiro14aki <hiro.1114.aki@gmail.com>
2025-02-25 00:31:54 +0900

    feat: Added code to display training images.

---
+++ chapter3/softmax.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def softmax_overflow(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c) # オーバーフロー対策
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
